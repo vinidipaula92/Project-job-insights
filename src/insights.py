6ce2c8a4675e38614774e8121999e7a5f6ba87b3
vinidipaula92 <vinidipaula92@gmail.com>
2022-10-20 16:26:40 -0300

    feat: :white_check_mark: Implementação de função para buscar o maior salário

--- src/insights.py
+++ src/insights.py
@@ -55,21 +55,13 @@ def filter_by_industry(jobs, industry):
 
 
 def get_max_salary(path):
-    """Get the maximum salary of all jobs
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The maximum salary paid out of all job opportunities
-    """
-    pass
+    content = read(path)
+    max_salary = {
+        int(job["max_salary"])
+        for job in content
+        if job["max_salary"].isdigit() and int(job["max_salary"]) > 0
+    }
+    return max(max_salary)
 
 
 def get_min_salary(path):
