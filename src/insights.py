b54709e2ab9121511d348ffd4ad9c47edbc0231a
vinidipaula92 <vinidipaula92@gmail.com>
2022-10-20 17:36:28 -0300

    feat: :white_check_mark: Implementação de função para filtrar os salarios e adicionando-os a lista propriamente dita

--- src/insights.py
+++ src/insights.py
@@ -65,18 +65,11 @@ def matches_salary_range(job, salary):
 
 
 def filter_by_salary_range(jobs, salary):
-    """Filters a list of jobs by salary range
-
-    Parameters
-    ----------
-    jobs : list
-        The jobs to be filtered
-    salary : int
-        The salary to be used as filter
-
-    Returns
-    -------
-    list
-        Jobs whose salary range contains `salary`
-    """
-    return []
+    content = []
+    for job in jobs:
+        try:
+            if matches_salary_range(job, salary):
+                content.append(job)
+        except ValueError:
+            print("Error: Invalid value")
+    return content
