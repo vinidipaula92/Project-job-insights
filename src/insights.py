935036a3bf310d518530bd20dd8e807950a23c94
vinidipaula92 <vinidipaula92@gmail.com>
2022-10-20 16:27:26 -0300

    feat: :white_check_mark: Implementação de função para buscar o menor salário

--- src/insights.py
+++ src/insights.py
@@ -65,21 +65,13 @@ def get_max_salary(path):
 
 
 def get_min_salary(path):
-    """Get the minimum salary of all jobs
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The minimum salary paid out of all job opportunities
-    """
-    pass
+    content = read(path)
+    min_salary = {
+        int(job["min_salary"])
+        for job in content
+        if job["min_salary"].isdigit() and int(job["min_salary"]) > 0
+    }
+    return min(min_salary)
 
 
 def matches_salary_range(job, salary):
