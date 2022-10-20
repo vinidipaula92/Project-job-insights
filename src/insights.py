21114c07036e7e17ff5eb3c797a8a68efcf11d37
vinidipaula92 <vinidipaula92@gmail.com>
2022-10-20 15:38:31 -0300

    feat: :white_check_mark: Implementação de função para receber dados do CSV
    Para leitura dos tipos de jobs vindos do CSV e retornando a uma lista

--- src/insights.py
+++ src/insights.py
@@ -1,3 +1,6 @@
+from src.jobs import read
+
+
 def get_unique_job_types(path):
     """Checks all different job types and returns a list of them
 
@@ -13,7 +16,11 @@ def get_unique_job_types(path):
     list
         List of unique job types
     """
-    return []
+    content = read(path)
+    job_types = []
+    for job in content:
+        job_types.append(job["job_type"])
+    return list(set(job_types))
 
 
 def filter_by_job_type(jobs, job_type):
