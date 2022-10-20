f4c6e55425c74989b60e04d621bd015b8ad29f49
vinidipaula92 <vinidipaula92@gmail.com>
2022-10-20 20:13:59 -0300

    feat: :sparkles: Implementação de rota para renderizar a página de um trabalho

--- src/routes_and_views.py
+++ src/routes_and_views.py
@@ -12,6 +12,7 @@ from .insights import (
 )
 
 from .more_insights import (
+    get_job,
     slice_jobs,
     get_int_from_args,
     build_jobs_urls,
@@ -72,6 +73,14 @@ def list_jobs():
     }
 
     return render_template("list_jobs.jinja2", ctx=ctx)
+
+
+@bp.route("/jobs/<index>")
+def job(index):
+    jobs = read("src/jobs.csv")
+    content = get_job(jobs, index)
+
+    return render_template("job.jinja2", job=content)
 
 
 def init_app(app: Flask):
