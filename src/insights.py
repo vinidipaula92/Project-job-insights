364ee353dc41b90646740e0c08900930e1162b52
vinidipaula92 <vinidipaula92@gmail.com>
2022-10-20 15:49:41 -0300

    style: :fire: Removendo códigos comentados sem uso

--- src/insights.py
+++ src/insights.py
@@ -2,20 +2,6 @@ from src.jobs import read
 
 
 def get_unique_job_types(path):
-    """Checks all different job types and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique job types
-    """
     content = read(path)
     job_types = []
     for job in content:
--- src/jobs.py
+++ src/jobs.py
@@ -4,18 +4,6 @@ from functools import lru_cache
 
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
     with open(path, "r") as file:
         content = csv.DictReader(file)
         new_content = list(content)
